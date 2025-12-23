00b1b123344b9b0107ec8f871fcc54b37143ee47
pookpikcraftpom <alittlesmoon@gmail.com>
2025-12-23 15:30:56 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo 
+set -eu
 
 log() { printf '%s\n' "$*"; }
 
@@ -101,4 +101,5 @@ log "=========================================="
 if [[ $APT_RC -ne 0 ]]; then
   log "Note: initial apt update returned code $APT_RC. See log: $APT_LOG"
 fi
+
 
